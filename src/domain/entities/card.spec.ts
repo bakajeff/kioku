5267f85644aed89ed63964bc9b54d1a6f72ad102
Jeffte Costa <jefftewf17@outlook.com>
2024-01-02 11:51:19 +0000

    test: return zero if there are no repetitions

--- src/domain/entities/card.spec.ts
+++ src/domain/entities/card.spec.ts
@@ -1,10 +1,12 @@
-import { describe, expect, it } from "vitest";
+import { beforeEach, describe, expect, it } from "vitest";
 
 import { Card } from "./card";
 
 describe("Card", () => {
 	it("should create a card with an id", () => {
-		const sut = new Card();
+		const sut = new Card({
+			repetitions: [],
+		});
 
 		expect(sut.id).toBeTruthy();
 	});
@@ -12,8 +14,21 @@ describe("Card", () => {
 	it("should create a card with the same passed id", () => {
 		const id = "valid id";
 
-		const sut = new Card(id);
+		const sut = new Card(
+			{
+				repetitions: [],
+			},
+			id,
+		);
 
 		expect(sut.id).toEqual(id);
+	});
+
+	it("should return zero if there are no repetitions", () => {
+		const sut = new Card({
+			repetitions: [],
+		});
+
+		expect(sut.repetitions).toBe(0);
 	});
 });
--- src/domain/entities/card.ts
+++ src/domain/entities/card.ts
@@ -1,13 +1,23 @@
 import { randomUUID } from "node:crypto";
 
+export interface CardProps {
+	repetitions: Date[];
+}
+
 export class Card {
 	private _id: string;
+	private props: CardProps;
 
-	constructor(id?: string) {
+	constructor(props: CardProps, id?: string) {
 		this._id = id ?? randomUUID();
+		this.props = props;
 	}
 
 	public get id(): string {
 		return this._id;
+	}
+
+	public get repetitions() {
+		return this.props.repetitions.length;
 	}
 }
