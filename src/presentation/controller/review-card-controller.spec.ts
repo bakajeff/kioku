cd5a4528aa2f41e165da72d7e3482a86f58888e1
Jeffte Costa <jefftewf17@outlook.com>
2024-01-06 08:39:45 +0000

    test: should return 400 if no cardId is provided

--- src/presentation/controller/review-card-controller.spec.ts
+++ src/presentation/controller/review-card-controller.spec.ts
@@ -35,4 +35,16 @@ describe("Review Card Controller", () => {
 			answer: 0,
 		});
 	});
+
+	it("should return 400 if no cardId is provided", async () => {
+		const reviewCardUseCase = new ReviewCardUseCaseSpy();
+		const sut = new ReviewCardController(reviewCardUseCase);
+
+		const response = await sut.handle({
+			cardId: "",
+			answer: 0,
+		});
+
+		expect(response.statusCode).toBe(400);
+	});
 });
--- src/presentation/controller/review-card-controller.ts
+++ src/presentation/controller/review-card-controller.ts
@@ -1,5 +1,5 @@
 import { ReviewCardUseCase } from "../../domain/interfaces/use-cases/review-card-use-case";
-import { ok } from "../helpers/http-helper";
+import { badRequest, ok } from "../helpers/http-helper";
 import { Controller } from "../interfaces/controller";
 import { HttpResponse } from "../interfaces/http-response";
 
@@ -10,6 +10,10 @@ export class ReviewCardController implements Controller {
 		cardId,
 		answer,
 	}: ReviewCardController.Request): Promise<HttpResponse> {
+		if (!cardId) {
+			return badRequest(Error("cardId is a required field"));
+		}
+
 		await this.reviewCardUseCase.execute({
 			cardId,
 			answer,
